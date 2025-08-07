3905563ade4af7ef64ee7e14301525ff557ebde9
Nikhil Lende <nikhillende9121@gmail.com>
2025-08-07 11:59:54 +0000

    pulled the code , same error

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,14 @@ export async function middleware(req: NextRequest) {
   const { user } = await verifyAuth();
   const { pathname } = req.nextUrl;
 
-  // If user is authenticated and is trying to access the login page, redirect them
-  if (user && pathname === '/') {
+  const isPublicPage = pathname === '/';
+
+  if (isPublicPage && user) {
     const redirectTo = user.role.name === 'Super Admin' ? '/super-admin/tenants' : '/products';
     return NextResponse.redirect(new URL(redirectTo, req.url));
   }
 
-  // If user is not authenticated and is trying to access a protected page, redirect to login
-  if (!user && pathname !== '/') {
+  if (!isPublicPage && !user) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
@@ -21,14 +21,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
